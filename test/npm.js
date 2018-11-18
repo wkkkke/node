8e91330a4532c971697bad89f40c4b482c2aa2de
wkkke <1621703096@qq.com>
2018-11-18 15:07:54 +0800

    npm

---
+++ test/npm.js
@@ -0,0 +1,7 @@
+//npm :包管理系统
+//使用时依赖shell环境
+//npm install@latest -g更新npm 
+//npm install -g 包名（安装在node_modules中）
+//npm 1s 查看有哪些包
+//可以采用cnpm 
+//package.json---npm init 更新包 npm update 卸载包npm uninstall
